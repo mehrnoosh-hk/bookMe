8e974c660ae47b7e51528a4c0e27d7ee8ffc18df
Mehrnoosh H. Kashani <mehrnoosh.haghighat@gmail.com>
2022-11-26 15:53:19 -0500

    add name validation to event

--- core/event.py
+++ core/event.py
@@ -3,6 +3,8 @@ from core.interval import Interval
 
 class Event:
     def __init__(self, name: str) -> None:
+        if not isinstance(name, str):
+            raise ValueError("Event only accepts strings as name")
         self.name = name
         self.intervals = []
 
